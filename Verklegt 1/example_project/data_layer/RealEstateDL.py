138bf5bcddfd93446aa59b369a3a5ff979934f7d
SiggiV2 <valursiggi@gmail.com>
2021-11-30 11:01:48 +0000

    Update RealEstateDL.py
    I started the work on Real Estate.

--- Verklegt 1/example_project/data_layer/RealEstateDL.py
+++ Verklegt 1/example_project/data_layer/RealEstateDL.py
@@ -1,11 +1,19 @@
+import csv
+
+
 class RealEstateDL:
     def __init__(self):
-        pass
+        self.filepath = "csv_files/RealEstate.csv"
 
-    def load_real_estate_from_file(self, RealEstatesID):
-        pass
+    def load_real_estate_from_file(self):
+        ret_list = []
+        with open(self.filepath, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                real = RealEstate(row["location"], row["street_number"], row["square_meters"], row["room"], row["type"], row["Property_number"])
+                ret_list.append(real)
+        return ret_listSS
 
     def store_real_to_file(self):
         pass
 
-    
