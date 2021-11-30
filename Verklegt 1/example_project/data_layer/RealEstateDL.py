7ab8117f411ae54634fa264c2e582248d26bb7fa
SiggiV2 <valursiggi@gmail.com>
2021-11-30 11:18:31 +0000

    Breytingar(Siggi)
    bætti við siggi_run.py og gerði svo nokrar breitingar í realEstete.py.

--- Verklegt 1/example_project/data_layer/RealEstateDL.py
+++ Verklegt 1/example_project/data_layer/RealEstateDL.py
@@ -6,12 +6,17 @@ class RealEstateDL:
         self.filepath = "csv_files/RealEstate.csv"
 
     def load_real_estate_from_file(self):
+        """[This function opens the file RealEstate.csv, reads it and returns a list.]
+        Returns:
+            [list]: [list of the Real estate]
+        """
         ret_list = []
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 real = RealEstate(row["location"], row["street_number"], row["square_meters"], row["room"], row["type"], row["Property_number"])
                 ret_list.append(real)
+                print(ret_list)
         return ret_listSS
 
     def store_real_to_file(self):
--- Verklegt 1/example_project/models/RealEstate.py
+++ Verklegt 1/example_project/models/RealEstate.py
@@ -1,5 +1,11 @@
 class RealEstate:
-    def __init__(self, realestate_id, amenities, conditions):
-        self.realestate_id = realestate_id
-        self.amenities = amenities
-        self.conditions = conditions
+    def __init__(self, location, street_number, square_meters, room, type, Property_number):
+        self.location = location
+        self.street_number = street_number
+        self.square_meters = square_meters
+        self.room = room
+        self.type = type
+        self.Property_number = Property_number
+
+    def __str__(self):
+        return f"Location: {self.location}, street number: {self.street_number}, square: {self.square_meters}, room: {self.room}, type: {self.type}, Property_number: {self.Property}"
