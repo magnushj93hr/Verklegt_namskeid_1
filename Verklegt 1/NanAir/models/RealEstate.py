7b9779a076852531e9f89ba82b4f2a48c68b5a89
magnushj93hr <89949774+magnushj93hr@users.noreply.github.com>
2021-11-30 11:32:26 +0000

    Maggi RealEstateDL
    Setti inn realestateDL

--- Verklegt 1/NanAir/models/RealEstate.py
+++ Verklegt 1/NanAir/models/RealEstate.py
@@ -1,7 +1,10 @@
 class RealEstate:
-    def __init__(self, realestate_id, amenities, conditions, type, rooms):
+    def __init__(self,address, realestate_id, amenities, conditions, type, rooms, location):
+        self.address = address
         self.realestate_id = realestate_id
         self.amenities = amenities
         self.conditions = conditions
         self.type = type
         self.rooms = rooms
+        self.location = location
+        
