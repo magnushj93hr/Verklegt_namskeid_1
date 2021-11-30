7a39f1b117bc5fc20296a930e54a28b9aa435376
magnushj93hr <89949774+magnushj93hr@users.noreply.github.com>
2021-11-30 11:13:44 +0000

    maggi emp
    employee update

--- Verklegt 1/NanAir/data_layer/EmployeeDL.py
+++ Verklegt 1/NanAir/data_layer/EmployeeDL.py
@@ -10,15 +10,15 @@ class EmployeeDL:
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                emp = Employee(row["name"], row["email"], row["phone"])
+                emp = Employee(row["Name"],row["ID"], row["Address"], row["Homeline"], row["Phone"],row["Email"], row["Location"])
                 ret_list.append(emp)
         return ret_list
 
     def create_employee(self, emp):
         with open(self.filepath, 'a', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["name","email","phone"]
+            fieldnames = ["name","ID","Address","Homeline","Phone","Email","Location"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'name': emp.name, "email": emp.email, "phone": emp.phone})
+            writer.writerow({'name': emp.name, "ID": emp.emp_id, "address": emp.address, "Homeline": emp.homeline, 'Phone': emp.phone, 'Email': emp.email, 'Location': emp.location})
 
     def load_employee_from_file(self, ID):
         pass
