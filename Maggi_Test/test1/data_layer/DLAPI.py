124521b6bc1ee9d3ea29cbbb15465149d26df2cc
HaukurS <89972635+HaukurS@users.noreply.github.com>
2021-12-02 13:52:35 +0000

    switcha i LL

--- Maggi_Test/test1/data_layer/DLAPI.py
+++ Maggi_Test/test1/data_layer/DLAPI.py
@@ -23,8 +23,8 @@ class DLAPI:
     def edit_employee(self, emp):
         return self.empDL.edit_employee(emp)
 
-    def emp_search(self, search_type, value):
-        return self.empDL.search(search_type, value)
+    # def emp_search(self, search_type, value):
+    #     return self.empDL.search(search_type, value)
     
     def get_all_realestate(self):
         return self.realDL.get_all_realestate()
