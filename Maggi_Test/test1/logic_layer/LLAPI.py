b90d4f214cb9b8094a44602a45c9b3d601074d2a
HaukurS <89972635+HaukurS@users.noreply.github.com>
2021-12-02 14:39:01 +0000

    search uptade

--- Maggi_Test/test1/logic_layer/LLAPI.py
+++ Maggi_Test/test1/logic_layer/LLAPI.py
@@ -33,8 +33,8 @@ class LLAPI:
     def edit_employee(self, emp):
         return self.empLL.edit_employee(emp)
 
-    def search(self, emp):
-        return self.empLL.search(emp)
+    # def search(self, emp):
+    #     return self.empLL.search(emp)
 
     def emp_search(self, search_type, value):
         return self.empLL.search(search_type, value)
