7a7c9f24da8ccd08bfe169231f06454a6e7cf921
Elly run <ellyrun@gmail.com>
2021-12-02 13:24:51 +0000

    search emp

--- Maggi_Test/test1/data_layer/EmployeeDL.py
+++ Maggi_Test/test1/data_layer/EmployeeDL.py
@@ -45,14 +45,15 @@ class EmployeeDL:
     
 
     def search(self, search_type, value):
+        fieldnames = ["name","id","address",'homeline','email','location','phone']
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = csv.DictReader(csvfile, fieldnames=fieldnames)
             for row in reader:
-                emp = Employee(row["name"], row["emp_id"], row["address"], row["homeline"], row["email"], row["location"], row["phone"])
+                emp = Employee(row["name"], row["id"], row["address"], row["homeline"], row["email"], row["location"], row["phone"])
                 if self.matches(emp, search_type, value):
-                    return "poop"
+                    return emp
 
     
-    def matches(emp, search_type, value):
+    def matches(self, emp, search_type, value):
         if search_type == "name" and value == emp.name:
             return True
