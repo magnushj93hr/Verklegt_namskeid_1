6dd6fdcd64efe1e86e0363f8a3ce8033df63275c
magnushj93hr <89949774+magnushj93hr@users.noreply.github.com>
2021-12-03 12:44:35 +0000

    creation date fix

--- Maggi_Test/test1/data_layer/CaseDL.py
+++ Maggi_Test/test1/data_layer/CaseDL.py
@@ -13,15 +13,15 @@ class CaseDL:
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                case = Case(row["id"],row["location"], row["subject"], row["description"], row["priority"], row["due date"], row["repeated"], row['date'])
+                case = Case(row["id"],row["location"], row["subject"], row["description"], row["priority"], row["repeated"], row['date'])
                 ret_list.append(case)
         return ret_list
 
     def create_case(self, case):
         with open(self.filepath, 'a', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["id", "location", "subject","description","priority",'due date','repeated','date']
+            fieldnames = ["id", "location", "subject","description","priority",'repeated','date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'id': case.id,'location': case.location, 'subject': case.subject, "description": case.description, "priority": case.priority, 'due date': case.due_date, 'repeated': case.repeated, 'date': case.date})
+            writer.writerow({'id': case.id,'location': case.location, 'subject': case.subject, "description": case.description, "priority": case.priority, 'repeated': case.repeated, 'date': case.date})
             
     def check_if_case_exists(self, id):
             with open(self.filepath, newline='', encoding='utf-8') as csvfile:
@@ -35,7 +35,7 @@ class CaseDL:
     def edit_case(self, case):
         temp_file = NamedTemporaryFile(mode = 'w', delete=False)
         
-        fieldnames = ["id","location","subject","description",'priority','due date','repeated','date']
+        fieldnames = ["id","location","subject","description",'priority','repeated','date']
         with open(self.filepath, 'r', newline='', encoding='utf-8') as csvfile, temp_file:
             reader = csv.DictReader(csvfile, fieldnames=fieldnames)
             #fieldnames = ["name","id","address",'homeline','email','location','phone']
@@ -45,9 +45,9 @@ class CaseDL:
             for row in reader:
                 if row['id'] == case.id:
                     print('updating row', row['id'])
-                    writer.writerow({'id': case.id,'location': case.location, "subject": case.subject, "description": case.description, 'priority': case.priority, 'due date': case.due_date, 'repeated': case.repeated, 'date': case.date})
+                    writer.writerow({'id': case.id,'location': case.location, "subject": case.subject, "description": case.description, 'priority': case.priority, 'repeated': case.repeated, 'date': row['date']})
                 else:
-                    row = {'id': row['id'],'location': row["location"], 'subject': row['subject'], 'description': row['description'], 'priority': row['priority'], 'due date': row['due date'], 'repeated': row['repeated'], 'date': row['date']}
+                    row = {'id': row['id'],'location': row["location"], 'subject': row['subject'], 'description': row['description'], 'priority': row['priority'], 'repeated': row['repeated'], 'date': row['date']}
                     writer.writerow(row)
 
         shutil.move(temp_file.name, self.filepath)
--- Maggi_Test/test1/models/Case.py
+++ Maggi_Test/test1/models/Case.py
@@ -1,13 +1,12 @@
 import datetime
 
 class Case:
-    def __init__(self, id, location, subject, description, priority, due_date, repeated, date = None):
+    def __init__(self, id, location, subject, description, priority, repeated, date = None):
         self.id = id
         self.location = location
         self.subject = subject
         self.description = description
         self.priority = priority
-        self.due_date = due_date
         self.repeated = repeated
         if date == None:
             self.date = self.creation_date()
@@ -15,7 +14,7 @@ class Case:
             self.date = date
 
     def __str__(self):
-        return f"id: {self.id}, location: {self.location}, subject: {self.subject}, description: {self.description}, priority: {self.priority}, due_date: {self.due_date}, repeated: {self.repeated}, creation date: {self.date}"
+        return f"id: {self.id}, location: {self.location}, subject: {self.subject}, description: {self.description}, priority: {self.priority}, repeated: {self.repeated}, creation date: {self.date}"
     
     def creation_date(self):
         dt = datetime.datetime.now()
