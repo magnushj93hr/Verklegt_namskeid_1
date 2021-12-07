c10cff147bf9783125131cea8dabac6c29d813ef
Elly run <ellyrun@gmail.com>
2021-12-07 15:06:41 +0000

    búa til get location name

--- Maggi_Test/test1/logic_layer/LLAPI.py
+++ Maggi_Test/test1/logic_layer/LLAPI.py
@@ -151,6 +151,9 @@ class LLAPI:
 
     def create_location(self,loc):
         return self.locLL.create_location(loc)
+    
+    def get_locations_name(self):
+        return self.locLL.get_locations_name()
 
 # ----------------------------------------------------------------
 
