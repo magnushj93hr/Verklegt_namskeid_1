fc33323975787669cfe225c71e0deb8f5f971689
Kristinhaf <kristin.h02@gmail.com>
2021-12-09 12:21:14 +0000

    commented functions in CaseDL

--- Maggi_Test/test1/data_layer/CaseDL.py
+++ Maggi_Test/test1/data_layer/CaseDL.py
@@ -9,6 +9,8 @@ class CaseDL:
         self.filepath = "Maggi_Test/test1/csv_files/Case.csv"
      
     def get_all_cases(self):
+        """Returns a list of all cases"""
+
         ret_list = []
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -18,29 +20,34 @@ class CaseDL:
         return ret_list
 
     def create_case(self, case):
+        """Takes in information you need to create a case. The information will 
+        be stored in a Case.csv file"""
+
         with open(self.filepath, 'a', newline='', encoding='utf-8') as csvfile:
             fieldnames = ["id", "location", "subject","description","priority",'repeated', "repeat_days", "real_est_id", 'emp_id', 'date', 'status', 'closed_date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'id': case.id,'location': case.location, 'subject': case.subject, "description": case.description, "priority": case.priority, 'repeated': case.repeated, "repeat_days": case.repeat_days, "real_est_id": case.real_est_id, 'emp_id': case.emp_id, 'date': case.date, 'status': case.status, 'closed_date': case.closed_date})
 
     def check_if_case_exists(self, id):
-            with open(self.filepath, newline='', encoding='utf-8') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    if row['id'] == id:
-                        return True
-                    else:
-                        return False
+        """Takes in the case id and checks if it exists, returns true if it exists else false"""
+
+        with open(self.filepath, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['id'] == id:
+                    return True
+                else:
+                    return False
 
     def edit_case(self, case):
+        """Takes in case information and updates it """
+
         temp_file = NamedTemporaryFile(mode = 'w', newline='', encoding='utf-8', delete=False)
         
         fieldnames = ["id","location","subject","description",'priority','repeated', "repeat_days", 'real_est_id', 'emp_id', 'date', 'status', 'closed_date']
         with open(self.filepath, 'r', newline='', encoding='utf-8') as csvfile, temp_file:
             reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-            #fieldnames = ["name","id","address",'homeline','email','location','phone']
             writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
-            # writer.writeheader()
 
             for row in reader:
                 if row['id'] == case.id:
--- Maggi_Test/test1/logic_layer/CaseLL.py
+++ Maggi_Test/test1/logic_layer/CaseLL.py
@@ -91,3 +91,4 @@ class CaseLL:
 
 if __name__ == "__main__":
     empLL = CaseLL(DLAPI())
+
