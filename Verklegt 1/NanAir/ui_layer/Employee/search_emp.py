77e1dac8b53110ee62cfa4dd40e33fcf09274b23
SiggiV2 <valursiggi@gmail.com>
2021-12-10 13:24:59 +0000

    Siggi
    fix Search emp

--- Verklegt 1/NanAir/ui_layer/Employee/search_emp.py
+++ Verklegt 1/NanAir/ui_layer/Employee/search_emp.py
@@ -15,7 +15,7 @@ class SearchEmp:
                     print("No employee found")
                 else:
                     self.print_searched_emp(result)
-            break
+            else: break
     
     def print_searched_emp(self, result):
         """Prints employee info"""
