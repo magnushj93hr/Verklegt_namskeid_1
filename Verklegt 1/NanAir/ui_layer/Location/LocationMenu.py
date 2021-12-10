d36d2211d52a9feba81e97049f338c1e403f6c8a
Elly run <ellyrun@gmail.com>
2021-12-10 12:48:46 +0000

    list locations

--- Verklegt 1/NanAir/ui_layer/Location/LocationMenu.py
+++ Verklegt 1/NanAir/ui_layer/Location/LocationMenu.py
@@ -1,12 +1,13 @@
 from ui_layer.Location.CreateLocation import CreateLocation
-
+from ui_layer.Location.list_all_locations import ListAllLocations
 
 class LocationMenu:
     def __init__(self, llapi, user):
         self.create_location = CreateLocation(llapi, user)
+        self.list_locations = ListAllLocations(llapi)
         self.llapi = llapi
         self.user = user
-        self.options = """
+        self.header = """
       __|__                                                                                             __|__
 *---o--(_)--o---*                                                                                 *---o--(_)--o---* 
 ___________________________________________________________________________________________________________________
@@ -14,18 +15,27 @@ ________________________________________________________________________________
 |       >Home(home)<        Employee(emp)        Real estate(real)         Cases(cases)        Contractor(con)    |
 |_________________________________________________________________________________________________________________|
 |                                                                                                                 |
-|       - 1         //Create new destination                        - r         //Return to previous menu         |
+|       - 1         //List all locations                                                                          |"""
+        
+        self.supervisor_opt = """|       - 2         //Create new location                                                                         |"""
+
+        self.footer = """|       - r         //Return to previous menu                                                                     |
 |_________________________________________________________________________________________________________________|
 """
 
     def draw_options(self):
-        print(self.options)
-        self.prompt_input()
+        print(self.header)
+        if self.user.is_supervisor():
+            print(self.supervisor_opt)
+        print(self.footer)
 
     def prompt_input(self):
         while True:
+            self.draw_options()
             command = input("Choose option: ")
             if command == "1":
+                self.list_locations.list_all_locations()
+            elif command == "2" and self.user.is_supervisor():
                 self.create_location.create_location()
             elif command == "r":
                 return "r"
@@ -33,5 +43,4 @@ ________________________________________________________________________________
                 print("invalid option, try again!")
             if command == "m":
                 return
-            print(self.options)
 
